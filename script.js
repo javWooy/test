e5ba09b5b2c153fbd09fcc4be1fc631d61c9ed30
javWooy <105819799+javWooy@users.noreply.github.com>
2023-11-13 16:07:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-
-// Example JavaScript to handle the subscription form submission
-document.addEventListener('DOMContentLoaded', () => {
-    const subscribeButton = document.querySelector('.hero button');
-    subscribeButton.addEventListener('click', () => {
-        // Implement the subscription logic here
-        alert('Thank you for subscribing!');
-    });
+// Handle subscription form submission
+document.querySelector('.subscription-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Perform the subscription logic
+    alert('Thank you for subscribing!');
 });
